ff39804fb7081d548466c6750c5fb102abfec132
Alina Sigaeva <52719567+asigaeva@users.noreply.github.com>
2022-01-06 15:35:44 +0100

    Update randomwalk.py

--- 02_01_Simulated_trajectories/Simple_cases/Random_walk/randomwalk.py
+++ 02_01_Simulated_trajectories/Simple_cases/Random_walk/randomwalk.py
@@ -3,12 +3,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 # Define parameters for the walk
-dims = 3
-step_n = 1200
-step_set = [-1, -.5, -.3, 0, .3, .5, 1]
-origin = np.zeros((1, dims))
+dims = 3 # number of dimensions
+step_n = 1200 # number of steps that comprise a trajectory
+step_set = [-1, -.5, -.3, 0, .3, .5, 1] # possible values of a step
+origin = np.zeros((1, dims)) # coordinates of the origin of the trajectory ((0,0,0))
 # Simulate steps in 3D
-for i in range (50):
+for i in range (50): # will create the set number of individual trajectory files
     step_shape = (step_n, dims)
     steps = np.random.choice(a=step_set, size=step_shape) # random choice of a step size in x, y, z (3 separate numbers) out of the step_set array
     path = np.concatenate([origin, steps]).cumsum(0)
@@ -16,13 +16,14 @@ for i in range (50):
     stop = path[-1:]
     x, y, z, t = [], [], [], []
 
-
+    # append the steps
     for set in path:
         x.append(set[0])
         y.append(set[1])
         z.append(set[2])
     t = list(range(step_n+1))
-
+    
+    # create and write the .tck file
     fname = "random_test_"+str(i)+".tck"
     f_out = open(fname, "w+")
     f_out.write('\t'.join(map(str, x))+'\n')
